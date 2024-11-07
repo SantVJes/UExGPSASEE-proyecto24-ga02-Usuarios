fa169b75656725947a66fc64abd2f1132dc90fa6
Jesus Santiago <Sanjesvel@outlook.com>
2024-11-07 14:55:14 +0100

    SR-30 implementacion-de-la-logica-de-autenticacion-y-validacion-de-credenciales
    SR-30 codijo de validacion de json
    
    SR-30 Codijo de validacion de parametros

--- openapi_server/controllers/usuario_controller.py
+++ openapi_server/controllers/usuario_controller.py
@@ -17,6 +17,29 @@ db = SQLAlchemy()
 def import_db_controller(database):
     global db
 
+    
+def iniciar_sesion():
+    
+    if not request.is_json:
+        return 'is magic'
+    
+    data = request.get_json()
+    # Extraer y validar campos obligatorios
+    email = data.get("email")
+    password = data.get("password")
+    
+    if not email or not password:
+        return 'is magic'
+    
+    # Intentar buscar el usuario en la base de datos
+    usuario = Usuario.query.filter_by(email=email).first()
+    
+    # Verificar si el usuario existe y la contraseña es correcta
+    if usuario and usuario.verificar_contraseña(password):  # Suponiendo que 'verificar_contraseña' es un método en Usuario
+        # Generar token o mensaje de éxito
+        return 'is magic '
+    
+    return 'is magic'
 
 def add_favorito(id_usuario, nombre_perfil, body):  # noqa: E501
     """Añadir un nuevo contenido al listado de favoritos de un perfil específico de un usuario por su ID
